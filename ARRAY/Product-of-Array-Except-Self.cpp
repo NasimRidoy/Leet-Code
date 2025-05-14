8ad1b1c68e78198dbbafd9039d3903a5ba06eb5c
Md. Nasim Ridoy <nasimhridoy695@gmail.com>
2025-05-14 14:58:28 +0600

    [238] [Time Beats: 38.48%] [Memory Beats: 58.70%] - LeetPush

--- ARRAY/Product-of-Array-Except-Self.cpp
+++ ARRAY/Product-of-Array-Except-Self.cpp
@@ -1,17 +1,21 @@
 class Solution {
 public:
     vector<int> productExceptSelf(vector<int>& nums) {
+        
         int sz = nums.size();
-        vector<int> prefix(sz), suffix(sz);
-        prefix[0]=1,suffix[sz-1]=1;
-        for (int i = 1,j=sz-2; i < sz && j>=0; i++,j--) {
-            prefix[i]=prefix[i-1]*nums[i-1];
-            suffix[j]=suffix[j+1]*nums[j+1];
-        }
+        //vector<int> prefix(sz), suffix(sz);
+        //prefix[0]=1,suffix[sz-1]=1;
         vector<int>res(sz);
-        for(int i=0;i<sz;i++)
+        int pref=1,suff=1;
+        for (int i = 0; i < sz; i++) {
+            res[i] = pref;
+            pref*=nums[i];
+        }
+        
+        for(int j=sz-1;j>=0;j--)
         {
-            res[i]=prefix[i]*suffix[i];
+            res[j]*= suff;
+            suff*=nums[j];
         }
         return res;
     }
